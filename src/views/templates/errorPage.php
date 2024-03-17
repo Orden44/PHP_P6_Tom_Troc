801ad5793e8d90c8e0754ada7b97b1a51df2734b
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-03-17 15:05:39 +0100

    updating template pages

--- src/views/templates/errorPage.php
+++ src/views/templates/errorPage.php
@@ -4,8 +4,8 @@
      */
 ?>
 
-<div class="error">
-    <h2>Erreur</h2>
+<div class="message">
+    <h1>Erreur</h1>
     <p><?= $errorMessage ?></p>
     <a href="index.php?action=home">Retour à la page d'accueil</a>
 </div>
